d853b27fa110fc7640c45e9924b3d224e661d676
mp371366 <mateuszp816@gmail.com>
2020-05-01 22:41:59 +0200

    Add form validation and display submit info.

--- index.ts
+++ index.ts
@@ -1,75 +1,101 @@
-let submitButton = document.querySelector('input[type=submit]') as HTMLElement;
-submitButton.style.display = 'none';
-
-let nameInput = document.querySelector('#name') as HTMLInputElement;
-console.log(nameInput.value);
-
-let delayHeader = document.querySelector('aside.problems h2') as HTMLElement;
-delayHeader.innerText = delayHeader.innerText.toUpperCase();
-
-let newFooter = document.createElement('p');
-newFooter.innerText = 'All rights reserved.';
-document.querySelector('body')?.appendChild(newFooter);
-
-setTimeout(() => {
-  console.log('At last.');
-}, 2000);
-
-function delay(time: number, handler: () => void): Promise<void> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      handler();
-      resolve();
-    }, time);
-  })
-}
-
-async function rainbow(el: HTMLElement) {
-  const time = 1000;
-  function setBackgroundColorAndWait(color: string): Promise<void> {
-    return delay(time, () => el.style.backgroundColor = color);
-  }
-
-  const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple'];
-  for (const color of colors) {
-    await setBackgroundColorAndWait(color);
-  }
-}
-
-rainbow(newFooter);
-
-const repoUrl = 'https://api.github.com/repos/Microsoft/TypeScript/commits';
-
-fetch(repoUrl)
-  .then((response) => response.json())
-  .then((data) => data[0].author.avatar_url)
-  .then(fetch)
-  .then((response) => response.blob())
-  .then((blob) => {
-    const img = document.createElement('img');
-    img.src = URL.createObjectURL(blob);
-    document.querySelector('body')?.appendChild(img);
-  })
-  .catch(console.error);
-
-function fib(x: number): number {
-  return x < 2 ? x : fib(x - 1) + fib(x - 2);
-}
-
-let i = 0;
-const delayMenu = document.querySelector('.problems') as HTMLElement;
-delayMenu.onclick = async () => {
-  i++;
-  console.log(fib(10 * i));
-
-  const currentColor = window.getComputedStyle(delayMenu).getPropertyValue('background-color');
-  const [, ...colorsAsText] = /rgb\((\d+),[^0-9]*(\d+),[^0-9]*(\d+)\)/.exec(currentColor) ?? [];
-  const [red, green, blue] = colorsAsText.map((colorAsText) => (parseInt(colorAsText, 10) + 0x20) % 256);
-
-  delayMenu.style.backgroundColor = `rgb(${red},${green},${blue})`;
-};
-
-const reservationForm  = document.querySelector('#reservation') as HTMLElement;
+const reservationForm  = document.querySelector('#reservation') as HTMLFormElement;
 reservationForm.onclick = (event: MouseEvent) => {
   event.stopPropagation();
+};
+
+let passengerName: string | null = null;
+let passengerSurname: string | null = null;
+let departure: string | null = null;
+let destination: string | null = null;
+let date: Date | null = null;
+
+function checkForm() {
+  const submitButton = document.querySelector('input[type=submit]') as HTMLInputElement;
+  submitButton.disabled = true;
+
+  if ((passengerName ?? '') === '') {
+    return;
+  }
+
+  if ((passengerSurname ?? '') === '') {
+    return;
+  }
+
+  if ((departure ?? '') === '') {
+    return;
+  }
+
+  if ((destination ?? '') === '') {
+    return;
+  }
+
+  if (date === null || date < new Date()) {
+    return;
+  }
+
+  submitButton.disabled = false;
+}
+
+checkForm();
+
+let nameInput = document.querySelector('#name') as HTMLInputElement;
+nameInput.onchange = () => {
+  passengerName = nameInput.value;
+  checkForm();
+};
+
+let surnameInput = document.querySelector('#surname') as HTMLInputElement;
+surnameInput.onchange = () => {
+  passengerSurname = surnameInput.value;
+  checkForm();
+};
+
+let departureInput = document.querySelector('#departure') as HTMLInputElement;
+departureInput.onchange = () => {
+  departure = departureInput.value;
+  checkForm();
+};
+
+let destinationInput = document.querySelector('#destination') as HTMLInputElement;
+destinationInput.onchange = () => {
+  destination = destinationInput.value;
+  checkForm();
+};
+
+let dateInput = document.querySelector('#date') as HTMLInputElement;
+dateInput.onchange = () => {
+  date = new Date(dateInput.value);
+  checkForm();
+};
+
+let modal = document.querySelector('.reservation-modal') as HTMLElement;
+modal.style.visibility = 'hidden';
+
+let form = document.querySelector('form') as HTMLFormElement;
+form.onsubmit = (ev: Event) => {
+  ev.preventDefault();
+  form.reset();
+
+  function makeInfo(info: string): HTMLElement {
+    let element = document.createElement('p');
+    element.innerText = info;
+    return element;
+  }
+
+  let nameInfo = makeInfo(`Name: ${passengerName}`);
+  let surnameInfo = makeInfo(`Surname: ${passengerSurname}`);
+  let departureInfo = makeInfo(`Departure: ${departure}`);
+  let destinationInfo = makeInfo(`Destination: ${destination}`);
+  let dateInfo = makeInfo(`Date: ${date?.toLocaleDateString()}`);
+
+  let modalBody = document.querySelector('.reservation-modal-data') as HTMLElement;
+  modalBody.innerHTML = '';
+  modalBody.append(nameInfo, surnameInfo, departureInfo, destinationInfo, dateInfo);
+
+  modal.style.visibility = 'visible';
+};
+
+let closeModal = document.querySelector('.close') as HTMLElement;
+closeModal.onclick = () => {
+  modal.style.visibility = 'hidden';
 };
