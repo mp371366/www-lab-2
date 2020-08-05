dae4573332cdb135388128d7bab64df507de780b
mp371366 <mp371366@github.com>
2020-08-05 12:51:43 +0200

    Add form logic and modal on submit.

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
-const reservationForm = document.querySelector('#reservation') as HTMLFormElement;
-reservationForm.onclick = (event: MouseEvent) => {
-  event.stopPropagation();
-};
+const form = document.querySelector('form') as HTMLFormElement;
+const nameInput = document.querySelector('#name') as HTMLInputElement;
+const surnameInput = document.querySelector('#surname') as HTMLInputElement;
+const departureInput = document.querySelector('#departure') as HTMLInputElement;
+const destinationInput = document.querySelector('#destination') as HTMLInputElement;
+const dateInput = document.querySelector('#date') as HTMLInputElement;
+const submitButton = document.querySelector('input[type=submit]') as HTMLInputElement;
+
+const modal = document.querySelector('.reservation-modal') as HTMLElement;
+const closeModal = document.querySelector('.close') as HTMLElement;
 
 let passengerName: string | null = null;
 let passengerSurname: string | null = null;
@@ -10,92 +16,75 @@ let destination: string | null = null;
 let date: Date | null = null;
 
 function checkForm() {
-  const submitButton = document.querySelector('input[type=submit]') as HTMLInputElement;
-  submitButton.disabled = true;
-
-  if ((passengerName ?? '') === '') {
-    return;
-  }
-
-  if ((passengerSurname ?? '') === '') {
-    return;
-  }
-
-  if ((departure ?? '') === '') {
-    return;
-  }
-
-  if ((destination ?? '') === '') {
-    return;
-  }
-
-  if (date === null || date < new Date()) {
-    return;
-  }
-
-  submitButton.disabled = false;
+  submitButton.disabled
+    = (passengerName ?? '') === ''
+    || (passengerSurname ?? '') === ''
+    || (departure ?? '') === ''
+    || (destination ?? '') === ''
+    || date === null || date < new Date();
 }
 
-checkForm();
-
-const nameInput = document.querySelector('#name') as HTMLInputElement;
-nameInput.onchange = () => {
-  passengerName = nameInput.value;
-  checkForm();
-};
-
-const surnameInput = document.querySelector('#surname') as HTMLInputElement;
-surnameInput.onchange = () => {
-  passengerSurname = surnameInput.value;
-  checkForm();
-};
-
-const departureInput = document.querySelector('#departure') as HTMLInputElement;
-departureInput.onchange = () => {
-  departure = departureInput.value;
-  checkForm();
-};
-
-const destinationInput = document.querySelector('#destination') as HTMLInputElement;
-destinationInput.onchange = () => {
-  destination = destinationInput.value;
-  checkForm();
-};
-
-const dateInput = document.querySelector('#date') as HTMLInputElement;
-dateInput.onchange = () => {
-  date = new Date(dateInput.value);
-  checkForm();
-};
-
-const modal = document.querySelector('.reservation-modal') as HTMLElement;
-modal.style.visibility = 'hidden';
-
-const form = document.querySelector('form') as HTMLFormElement;
-form.onsubmit = (ev: Event) => {
-  ev.preventDefault();
+function clearForm() {
+  passengerName = passengerSurname = departure = destination = date = null;
   form.reset();
+  checkForm();
+}
 
-  function makeInfo(info: string): HTMLElement {
-    const element = document.createElement('p');
-    element.innerText = info;
-    return element;
-  }
+function init() {
+  nameInput.onchange = () => {
+    passengerName = nameInput.value;
+    checkForm();
+  };
 
-  const nameInfo = makeInfo(`Name: ${passengerName}`);
-  const surnameInfo = makeInfo(`Surname: ${passengerSurname}`);
-  const departureInfo = makeInfo(`Departure: ${departure}`);
-  const destinationInfo = makeInfo(`Destination: ${destination}`);
-  const dateInfo = makeInfo(`Date: ${date?.toLocaleDateString()}`);
+  surnameInput.onchange = () => {
+    passengerSurname = surnameInput.value;
+    checkForm();
+  };
 
-  const modalBody = document.querySelector('.reservation-modal-data') as HTMLElement;
-  modalBody.innerHTML = '';
-  modalBody.append(nameInfo, surnameInfo, departureInfo, destinationInfo, dateInfo);
+  departureInput.onchange = () => {
+    departure = departureInput.value;
+    checkForm();
+  };
 
-  modal.style.visibility = 'visible';
-};
+  destinationInput.onchange = () => {
+    destination = destinationInput.value;
+    checkForm();
+  };
 
-const closeModal = document.querySelector('.close') as HTMLElement;
-closeModal.onclick = () => {
-  modal.style.visibility = 'hidden';
-};
+  dateInput.onchange = () => {
+    date = new Date(dateInput.value);
+    checkForm();
+  };
+
+
+  form.onsubmit = (ev: Event) => {
+    ev.preventDefault();
+
+    function makeInfo(info: string): HTMLElement {
+      const element = document.createElement('p');
+      element.innerText = info;
+      return element;
+    }
+
+    const nameInfo = makeInfo(`Name: ${passengerName}`);
+    const surnameInfo = makeInfo(`Surname: ${passengerSurname}`);
+    const departureInfo = makeInfo(`Departure: ${departure}`);
+    const destinationInfo = makeInfo(`Destination: ${destination}`);
+    const dateInfo = makeInfo(`Date: ${date?.toLocaleDateString()}`);
+
+    const modalBody = document.querySelector('.reservation-modal-data') as HTMLElement;
+    modalBody.innerHTML = '';
+    modalBody.append(nameInfo, surnameInfo, departureInfo, destinationInfo, dateInfo);
+
+    modal.style.visibility = 'visible';
+  };
+
+  closeModal.onclick = () => {
+    modal.style.visibility = 'hidden';
+    clearForm();
+  };
+
+  clearForm();
+}
+
+init();
